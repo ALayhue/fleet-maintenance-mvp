ec43fdd6bbf31232c5da396e25b6adf5d1517285
ALayhue <austin.layhue@bldr.com>
2025-08-27 21:09:47 -0400

    Update db.js

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@
 const Database = require('better-sqlite3');
 const path = require('path');
 const bcrypt = require('bcryptjs');
-const dbPath = path.join(__dirname, '..', 'data.sqlite');
+const dbPath = process.env.DB_PATH || path.join(__dirname, '..', 'data.sqlite');
 const db = new Database(dbPath);
 
 function initDb() {
